ec8b2120de96c25f67eea4e52178b7e5277a2a96
sky13g <48208531+sky13g@users.noreply.github.com>
2026-07-06 16:42:06 +0530

    Update sharekhan-trading-algo.py

--- sharekhan-trading-algo.py
+++ sharekhan-trading-algo.py
@@ -121,7 +121,7 @@ def send_telegram_alert(message):
     if not TELEGRAM_TOKEN or not CHAT_ID:  
         print(f"[Telegram Log]: {message}")  
         return  
-    url = f"https://telegram.org{TELEGRAM_TOKEN}/sendMessage"  
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"  
     payload = {"chat_id": CHAT_ID, "text": message, "parse_mode": "Markdown"}  
     try: 
         requests.post(url, json=payload, timeout=5)
